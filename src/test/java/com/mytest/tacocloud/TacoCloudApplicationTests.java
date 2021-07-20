eb4d574fdbdbc50e831a20228fdc2b6612254e95
leelixiangjun <leelixiangjun@didiglobal.com>
2021-07-20 17:56:46 +0800

    init

---
+++ src/test/java/com/mytest/tacocloud/TacoCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mytest.tacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
